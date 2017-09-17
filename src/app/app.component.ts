b4206639534d246d1377b2afa0cd4b771d2f21e5
jsongo <jsongo@qq.com>
2017-09-17 15:31:49 +0800

    第一个可运行版本

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = '图片列表展示';
 }
--- src/app/pic-list/pic-list.component.ts
+++ src/app/pic-list/pic-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PicData } from '../pic/pic.model';
 import { dataList } from '../data';
 
-
 @Component({
   selector: 'pic-list',
   templateUrl: './pic-list.component.html',
